52ef0e58efa139647c8081f5adc6d2fdd7f3ca1a
ptraversi <c_prescott.traversi@viasat.com>
2020-12-10 08:12:32 -0800

    fix: formatting on output assertion errors, update string strcmp function check, & rename testing executable extension file

--- src/assert.c
+++ src/assert.c
@@ -11,15 +11,15 @@
 
 int AssertNumber(int actual, int expected) {
   if (actual != expected) {
-    printf("Actual: %d, Expected: %d", actual, expected);
+    printf("Actual: %d, Expected: %d\n", actual, expected);
     return -1;
   }
   return 0;
 }
 
 int AssertString(char actual[], char expected[]) {
-  if (!strcmp(actual, expected)) {
-    printf("Actual: %s, Expected: %s", actual, expected);
+  if (strcmp(actual, expected) != 0) {
+    printf("Actual: %s, Expected: %s\n", actual, expected);
     return -1;
   }
   return 0;
@@ -29,7 +29,7 @@ int AssertArray(int actual[], int expected[], int n) {
   int i = 0;
   while (i < n) {
     if (actual[i] != expected[i]) {
-      printf("First incorrect index at  %d. Actual: %d, Expected: %d", i, actual[i], expected[i]);
+      printf("First incorrect index at  %d. Actual: %d, Expected: %d\n", i, actual[i], expected[i]);
       return -1;
     }
     i++;
